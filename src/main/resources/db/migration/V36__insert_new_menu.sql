2238ca085cec4551d130d6d89ecdc6da3e0a4089
lacau <lacau@zpay.com.br>
2020-12-04 22:34:31 -0300

    Add new menu perspectives

---
+++ src/main/resources/db/migration/V36__insert_new_menu.sql
@@ -0,0 +1,2 @@
+insert into view_data (key, content_en, content_pt, "scope") values('shared.menu.5.title', 'perspectives', 'perspectivas', 'MENU');
+insert into view_data (key, content_en, content_pt, "scope") values('shared.menu.5.text', 'Follow our<br>work', 'Acompanhe nosso<br>trabalho', 'MENU');
